ca3a1540cde8fb6e23fa0cb73b4edf9bec652099
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-09-15 08:51:30 +0530

    Basic Error Condition Checks

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+
+const btn_check = document.querySelector(".check");
+const message = document.querySelector(".message");
+const guess = document.querySelector(".guess");
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 20) + 1;
+};
+
+let answer = getRandomNumber();
+
+btn_check.addEventListener("click", function () {
+  let guessNum = guess.value;
+
+  if (!guessNum) {
+    message.textContent = "No Number";
+  } else if (guessNum > 20) {
+    message.textContent = "Guess Greater then 20";
+  } else if (guessNum < 1) {
+    message.textContent = "Guess Lesser then 1";
+  } 
+});

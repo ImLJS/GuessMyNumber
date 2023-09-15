7d119e53e3d566033d1bf7948ec72057e0ce54a9
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-09-15 10:43:23 +0530

    Winner and Loser Conditions

--- script.js
+++ script.js
@@ -3,8 +3,12 @@
 const btn_check = document.querySelector(".check");
 const message = document.querySelector(".message");
 const guess = document.querySelector(".guess");
+const score = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
+const number = document.querySelector(".number");
+const bgcolor = document.querySelector("body").style;
 
-
+let curScore = 20;
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 20) + 1;
 };
@@ -17,17 +21,27 @@ btn_check.addEventListener("click", function () {
 
   if (!guessNum) {
     message.textContent = "No Number";
-  } else if (guessNum > 20) {
-    message.textContent = "Guess Greater then 20";
-  } else if (guessNum < 1) {
-    message.textContent = "Guess Lesser then 1";
-  } else{
-    if(Number(guessNum)===answer){
+  } else if (curScore > 1) {
+    if (Number(guessNum) === answer) {
       message.textContent = "Correct";
-    } else if(guessNum>answer){
+      number.textContent = answer;
+      highscore.textContent = curScore;
+      bgcolor.backgroundColor = '#60b347';
+      number.style.width = '30rem';
+    } else if (guessNum > answer) {
       message.textContent = "Too High!";
-    } else{
+      curScore -= 1;
+      score.textContent = curScore;
+    } else {
       message.textContent = "Too Low";
+      curScore -= 1;
+      score.textContent = curScore;
     }
+  } else {
+    message.textContent = "You Lost The Game";
+    curScore -= 1;
+    score.textContent = curScore;
+    number.textContent = answer;
+    bgcolor.backgroundColor = 'red';
   }
 });

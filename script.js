ed09d2d3decdd0eebfa0bea3b84d04823cdb809e
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-09-15 11:04:54 +0530

    Game Reset and HighScore Implementation

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ const score = document.querySelector(".score");
 const highscore = document.querySelector(".highscore");
 const number = document.querySelector(".number");
 const bgcolor = document.querySelector("body").style;
+const reset = document.querySelector(".again");
 
 let curScore = 20;
+let curHighScore = 0;
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 20) + 1;
 };
+
+function resetGame() {
+  bgcolor.backgroundColor = "#222";
+  curScore = 20;
+  answer = getRandomNumber();
+  console.log(answer);
+  guess.value = "";
+  number.textContent = "?";
+  number.style.width = "15rem";
+  message.textContent = 'Start guessing...'
+}
 
 let answer = getRandomNumber();
 console.log(answer);
@@ -25,9 +38,12 @@ btn_check.addEventListener("click", function () {
     if (Number(guessNum) === answer) {
       message.textContent = "Correct";
       number.textContent = answer;
-      highscore.textContent = curScore;
-      bgcolor.backgroundColor = '#60b347';
-      number.style.width = '30rem';
+      curHighScore = curScore;
+      bgcolor.backgroundColor = "#60b347";
+      number.style.width = "30rem";
+
+      if (curHighScore > highscore.textContent)
+        highscore.textContent = curHighScore;
     } else if (guessNum > answer) {
       message.textContent = "Too High!";
       curScore -= 1;
@@ -42,6 +58,10 @@ btn_check.addEventListener("click", function () {
     curScore -= 1;
     score.textContent = curScore;
     number.textContent = answer;
-    bgcolor.backgroundColor = 'red';
+    bgcolor.backgroundColor = "red";
   }
+});
+
+reset.addEventListener("click", function () {
+  resetGame();
 });

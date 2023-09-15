d8c67f2e6b10049ad1770c576629704f82431f2a
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-09-15 10:16:55 +0530

    Condition Checks

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const btn_check = document.querySelector(".check");
 const message = document.querySelector(".message");
 const guess = document.querySelector(".guess");
 
+
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 20) + 1;
 };
 
 let answer = getRandomNumber();
+console.log(answer);
 
 btn_check.addEventListener("click", function () {
   let guessNum = guess.value;
@@ -19,5 +21,13 @@ btn_check.addEventListener("click", function () {
     message.textContent = "Guess Greater then 20";
   } else if (guessNum < 1) {
     message.textContent = "Guess Lesser then 1";
-  } 
+  } else{
+    if(Number(guessNum)===answer){
+      message.textContent = "Correct";
+    } else if(guessNum>answer){
+      message.textContent = "Too High!";
+    } else{
+      message.textContent = "Too Low";
+    }
+  }
 });
